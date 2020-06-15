dea8490ee7c01c721b13fc4856353a91aee6ed57
BenjaminGustafson <bgustafs@trinity.edu>
2020-06-15 10:10:17 -0500

    added random scene function

--- main.cpp
+++ main.cpp
@@ -29,10 +29,58 @@ color ray_color(const ray& r, const hittable& world, int depth){
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    for(int a = -11; a < 11; a++){
+        for(int b = -11; b < 11; b++){
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
+
+
 int main() {
     //Setup iamge size
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 384;
+    const auto aspect_ratio = 3.0 / 2.0;
+    const int image_width = 120;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
@@ -40,18 +88,13 @@ int main() {
     //Write file heading
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    hittable_list world;
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5, make_shared<lambertian>(color(.1, .2, .5))));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, make_shared<lambertian>(color(.8,.8,0.))));
-    world.add(make_shared<sphere>(point3(1,0,-1), 0.5, make_shared<metal>(color(.8, .6, .2), 0.3)));
-    world.add(make_shared<sphere>(point3(-1,0,-1), 0.5, make_shared<dielectric>(1.5)));
-    world.add(make_shared<sphere>(point3(-1,0,-1), -0.45, make_shared<dielectric>(1.5)));
+    hittable_list world = random_scene();
 
-    point3 lookfrom(3,3,2);
-    point3 lookat(0,0,-1);
+    point3 lookfrom(13,2,3);
+    point3 lookat(0,0,0);
     vec3 vup(0,1,0);
-    auto dist_to_focus = (lookfrom-lookat).length();
-    auto aperture = 2.0;
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
 
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
@@ -62,9 +105,10 @@ int main() {
     //      find the color of that ray
     //      write the color to the image
     for(int j = image_height-1; j >= 0; j--){
-        double percent = 100 - 100*double(j)/image_height;
-        std::cerr << "\r Rendering " << percent << "% " << std::flush;
+        double row_percent = 100 - 100*double(j)/image_height;
         for(int i = 0; i < image_width; i++){
+            double col_percent = 100*double(i)/image_width;
+            std::cerr << "\r Rows: " << row_percent << "% Columns: " << col_percent << "%    " << std::flush;
             color pixel_color(0, 0, 0);
             for(int s = 0; s < samples_per_pixel; s++){
                 auto u = (i + random_double()) / (image_width-1);
